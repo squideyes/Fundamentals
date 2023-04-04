750fbdfd37dfa8b2387c59b626a77ff754dc0514
Louis S. Berman <louis@squideyes.com>
2023-04-04 11:51:03 -0400

    Removed dependency on resource files

--- UnitTests/Helpers/CreditCardHelperTests.cs
+++ UnitTests/Helpers/CreditCardHelperTests.cs
@@ -19,23 +19,37 @@ public class CreditCardHelperTests
         public required string[] Visa { get; init; }
     }
 
-    [Fact]
-    public void From_ValidInput_Constructs()
-    {
-        static void Validate(string[] cardNumbers)
-        {
-            foreach (var cardNumber in cardNumbers)
-                CreditCardHelper.IsNumber(cardNumber).Should().BeTrue();
-        }
-
-        var validCards = JsonSerializer.Deserialize<ValidCards>(
-            Properties.Resources.ValidCards);
-
-        Validate(validCards!.Amex);
-        Validate(validCards!.Discover);
-        Validate(validCards!.MasterCard);
-        Validate(validCards!.Visa);
-    }
+    [Theory]
+    [InlineData("4012 8888 8888 1881")]
+    [InlineData("4111 1111 1111 1111")]
+    [InlineData("4917 6100 0000 0000")]
+    [InlineData("5274 5763 9425 9961")]
+    [InlineData("5555 5555 5555 4444")]
+    [InlineData("5105 1051 0510 5100")]
+    [InlineData("3714 496353 98431")]
+    [InlineData("3787 344936 71000")]
+    [InlineData("3400 000000 00009")]
+    [InlineData("3411 111111 11111")]
+    [InlineData("3434 343434 34343")]
+    [InlineData("3468 276304 35344")]
+    [InlineData("3700 000000 00002")]
+    [InlineData("3700 002000 00000")]
+    [InlineData("3704 072699 09809")]
+    [InlineData("3705 560193 09221")]
+    [InlineData("3742 000000 00004")]
+    [InlineData("3764 622809 21451")]
+    [InlineData("3777 527498 96404")]
+    [InlineData("3782 822463 10005")]
+    [InlineData("6011 1111 1111 1117")]
+    [InlineData("6011 0009 9013 9424")]
+    [InlineData("6011 0000 0000 0004")]
+    [InlineData("6011 0004 0000 0000")]
+    [InlineData("6011 1532 1637 1980")]
+    [InlineData("6011 6011 6011 6611")]
+    [InlineData("6011 6874 8256 4166")]
+    [InlineData("6011 8148 3690 5651")]
+    public void From_ValidInput_Constructs(string cardNumber) =>
+        CreditCardHelper.IsNumber(cardNumber).Should().BeTrue();
 
     [Theory]
     [InlineData("4922986976940144")]
