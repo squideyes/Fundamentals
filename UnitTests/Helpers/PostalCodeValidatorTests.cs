94ec2af90da9b062fc50643fc51c77f3dc3c0085
Louis S. Berman <louis@squideyes.com>
2023-04-05 17:05:40 -0400

    Fixed PostalCodeValidator.IsPostalCode bug

---
+++ UnitTests/Helpers/PostalCodeValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using static SquidEyes.Fundamentals.PostalCodeValidator;
+
+namespace SquidEyes.UnitTests;
+
+public class PostalCodeValidatorTests
+{
+    [Theory]
+    [InlineData("12345", "US", true)]
+    [InlineData("A", "US", false)]
+    [InlineData("12345", "XX", false)]
+    public void IsPostalCode_InvalidInput_ReturnsFalse(
+        string code, string country, bool expected)
+    {
+        IsPostalCode(code, country).Should().Be(expected);
+    }
+}
