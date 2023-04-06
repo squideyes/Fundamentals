7b2cfecf7da3b58d1a71ca778ac80394c9f627e6
Louis S. Berman <louis@squideyes.com>
2023-04-06 10:16:04 -0400

    Updated ValueSet and publish.yml

--- Fundamentals/Collections/ValueSet.cs
+++ Fundamentals/Collections/ValueSet.cs
@@ -4,16 +4,15 @@
 // ********************************************************
 
 using System.Numerics;
-using System.Text.Json.Nodes;
 
 namespace SquidEyes.Fundamentals;
 
 public class ValueSet
 {
     private readonly Type type;
-    private readonly List<IConvertible> values;
+    private readonly List<object> values;
 
-    private ValueSet(Type type, List<IConvertible> values)
+    private ValueSet(Type type, List<object> values)
     {
         this.type = type;
         this.values = values;
@@ -26,154 +25,77 @@ public class ValueSet
         return values.Cast<T>().ToList();
     }
 
-    public static ValueSet From(int min, int max, int step) =>
-        From<int>(min, max, step);
-
-    public static ValueSet From(double min, double max, double step) =>
-        From<double>(min, max, step);
-
-    private static ValueSet From<T>(T min, T max, T step)
-        where T : INumber<T>, IConvertible
-    {
-        if (max <= min)
-            throw new ArgumentOutOfRangeException(nameof(max));
-
-        if (step <= T.Zero || step > (max - min))
-            throw new ArgumentOutOfRangeException(nameof(step));
-
-        var values = new List<IConvertible>();
-
-        for (T value = min; value < max; value += step)
-            values.Add(value);
-
-        return new ValueSet(typeof(T), values);
-    }
-
     public static ValueSet From<T>(params T[] values)
-        where T : IConvertible
+        where T : Enum
     {
         return From(values.ToList());
     }
 
     public static ValueSet From<T>(IEnumerable<T> values)
-        where T : IConvertible
+        where T : Enum
     {
-        if (values == null)
-            throw new ArgumentNullException(nameof(values));
+        values.Any().Must().Be(true);
+        values.Must().Be(v => v.IsUnique());
 
-        if (!values.Any() || values.Any(v => v == null))
-            throw new ArgumentOutOfRangeException(nameof(values));
-
-        values.Must().Be(v => IsValueSetType(values));
-
-        return new ValueSet(
-            typeof(T), values.Cast<IConvertible>().ToList());
+        return new(typeof(T), values.Cast<object>().ToList());
     }
 
-    internal static (string Name, ValueSet ValueSet) From(JsonNode node)
+    public static ValueSet From(params int[] values) =>
+        From(values);
+
+    public static ValueSet From(IEnumerable<int> values)
     {
-        var name = node!["name"]!.GetValue<string>();
+        values.Any().Must().Be(true);
+        values.Must().Be(v => v.IsUnique());
 
-        var type = Type.GetType(node!["type"]!.GetValue<string>());
-
-        (T, T, T) GetMinMaxStep<T>() where T : struct, IConvertible
-        {
-            var min = node!["min"]!.GetValue<T>();
-            var max = node!["max"]!.GetValue<T>();
-            var step = node!["step"]!.GetValue<T>();
-
-            return (min, max, step);
-        }
-
-        if (type!.IsEnum)
-            return (name, new ValueSet(type, GetEnums(node, type)));
-
-        var values = new List<IConvertible>();
-
-        switch (type.FullName)
-        {
-            case "System.Bool":
-                values.AddRange(new HashSet<IConvertible>(node!["values"]!
-                    .AsArray().Select(v => (IConvertible)v!.GetValue<bool>())));
-                return (name, new ValueSet(type, values));
-            case "System.Int32":
-                values = GetValues<int>(node);
-                if (values.Count == 0)
-                {
-                    var (min, max, step) = GetMinMaxStep<int>();
-                    for (var value = min; value <= max; value += step)
-                        values.Add(value);
-                }
-                return (name, new ValueSet(type, values));
-            case "System.Double":
-                values = GetValues<double>(node);
-                if (values.Count == 0)
-                {
-                    var (min, max, step) = GetMinMaxStep<double>();
-                    for (var value = min; value <= max; value += step)
-                        values.Add(value);
-                }
-                return (name, new ValueSet(type, values));
-            default:
-                throw new ArgumentOutOfRangeException(nameof(type));
-        }
+        return new(typeof(int), values.Cast<object>().ToList());
     }
 
-    private static bool IsValueSetType<T>(T value)
+    public static ValueSet From(int min, int max, int step)
     {
-        if (value!.GetType().GetGenericArguments()[0].IsEnum)
-            return true;
+        ValidateMinMaxStep(min, max, step);
 
-        return value switch
-        {
-            IEnumerable<bool> _ => true,
-            IEnumerable<int> _ => true,
-            IEnumerable<double> _ => true,
-            _ => false
-        };
-    }
+        var values = new List<object>();
 
-    private static List<IConvertible> GetEnums(JsonNode node, Type type)
-    {
-        if (!type!.IsEnum)
-            throw new ArgumentOutOfRangeException(nameof(type));
-
-        var values = new List<IConvertible>();
-
-        var n = node!["values"]!;
-
-        if (n == null)
-            return values;
-
-        foreach (var e in n.AsArray().Select(v => v!.GetValue<string>()))
-            values.Add((IConvertible)Enum.Parse(type!, e));
-
-        return values;
-    }
-
-    private static List<IConvertible> GetValues<T>(JsonNode node)
-        where T : struct, IConvertible
-    {
-        var values = new List<IConvertible>();
-
-        var n = node!["values"]!;
-
-        if (n == null)
-            return values;
-
-        foreach (var value in n.AsArray().Select(v => v!.GetValue<T>()))
+        for (int value = min; value < max; value += step)
             values.Add(value);
 
-        return values;
+        return new ValueSet(typeof(int), values);
     }
 
-    private static List<IConvertible> GetStrings(JsonNode parent)
-    {
-        var node = parent!["values"]!;
+    public static ValueSet From(params double[] values) =>
+        From(values);
 
-        return node == null
-            ? throw new ArgumentNullException(nameof(parent))
-            : node.AsArray().Select(
-            v => (IConvertible)v!.GetValue<string>()).ToList();
+    public static ValueSet From(IEnumerable<double> values)
+    {
+        values.Any().Must().Be(true);
+        values.Must().Be(v => v.IsUnique());
+
+        return new(typeof(double), values.Cast<object>().ToList());
+    }
+
+    public static ValueSet From(
+        double min, double max, double step, int? digits = null)
+    {
+        ValidateMinMaxStep(min, max, step);
+
+        var values = new List<object>();
+
+        for (double value = min; value < max; value += step)
+        {
+            if (digits.HasValue)
+                values.Add(double.Round(value, digits.Value));
+            else
+                values.Add(value);
+        }
+
+        return new ValueSet(typeof(double), values);
+    }
+
+    private static void ValidateMinMaxStep<T>(T min, T max, T step)
+        where T : INumber<T>
+    {
+        max.Must().BeGreaterThan(min);
+        step.Must().Be(v => v > T.Zero && step <= (max - min));
     }
 }
---
+++ UnitTests/Collections/ValueSetTests.cs
@@ -0,0 +1,14 @@
+﻿using SquidEyes.Fundamentals;
+
+namespace SquidEyes.UnitTests;
+
+public class ValueSetTests
+{
+    [Fact]
+    public void X()
+    {
+        var ints = ValueSet.From(1, 2, 3);
+        var doubles = ValueSet.From(1.0, 2.0, 3.0);
+        var floats = ValueSet.From(1.0f, 2.0f, 3.0f);
+    }
+}
