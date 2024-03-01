f2b745aca246d99f22e15b7eb8b8fc48c976ee48
Louis S. Berman <louis@squideyes.com>
2024-02-29 23:13:47 -0500

    Updated code headers

--- Fundamentals/Config/ConfigBase.cs
+++ Fundamentals/Config/ConfigBase.cs
@@ -1,4 +1,9 @@
-﻿using ErrorOr;
+﻿// ********************************************************
+// The use of this source code is licensed under the terms
+// of the MIT License (https://opensource.org/licenses/MIT)
+// ********************************************************
+
+using ErrorOr;
 using FluentValidation.Results;
 
 namespace SquidEyes.Fundamentals;
