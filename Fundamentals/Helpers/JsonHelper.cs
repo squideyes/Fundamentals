39d7d59ac7000c0afe424f72aa0d269acc8ff9f7
Louis S. Berman <louis@squideyes.com>
2024-09-10 09:26:00 -0400

    Added JsonHelper

---
+++ Fundamentals/Helpers/JsonHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SquidEyes.Fundamentals;
+
+public static class JsonHelper
+{
+    public static JsonSerializerOptions GetJsonSerializerOptions(
+        bool indented = false, Action<JsonSerializerOptions> configure = null!)
+    {
+        var options = new JsonSerializerOptions()
+        {
+            AllowTrailingCommas = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = indented
+        };
+
+        options.Converters.Add(new JsonStringEnumConverter());
+
+        if (configure is not null)
+            configure(options);
+
+        return options;
+    }
+}
